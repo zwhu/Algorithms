0c0d56b7a810caf25904c7212a27c17effaba703
胡正伟 <331946763@qq.com>
2015-10-28 07:32:45 +0800

    1. 二分查找 ----> 针对有序数组 2. 缺少二分查找的测试用例

---
+++ binarysearch.js
@@ -0,0 +1,18 @@
+/**
+ * Created by zwhu on 15/10/28.
+ * 二分查找
+ */
+
+let binarySearch = (key, a) => {
+  let lo = 0
+    , hi = a.length - 1
+  while (lo <= hi) {
+    let mid = ((lo + hi) / 2) | 0
+    if (a[mid] < key)
+      hi = mid - 1
+    else if (a[mid] > key)
+      lo = mid + 1
+    else return mid
+  }
+  return -1
+}
