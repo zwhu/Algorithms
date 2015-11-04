828069d7b4ddb42e9ef0de24f8327a6b939135b9
胡正伟 <331946763@qq.com>
2015-11-04 20:13:49 +0800

    生成随机数组的方法

---
+++ selection.js
@@ -0,0 +1,44 @@
+/**
+ * Created by zwhu on 15/10/31.
+ * 生成随机数组
+ */
+//
+let rangeArray = (start, end) => Array(end - start + 1).fill(0).map((v, i) => i + start)
+
+let randomArray1 = (start, end) => {
+  let range = rangeArray(start, end)
+    , random = []
+    , N = range.length
+
+
+  while (N--) {
+    let i = Math.random() * (N + 1) | 0
+    random.push(range[i])
+    range[i] = range[N]
+    //range.splice(i, 1)
+  }
+  return random
+}
+
+let randomArray2 = (start, end) => {
+  let o = {}
+    , length = end - start + 1
+    , random = []
+  while (random.length < length) {
+    let i = (Math.random() * length + 1) | 0
+    if (o[i]) continue
+    else {
+      o[i] = true
+      random.push(i)
+    }
+
+  }
+  return random
+}
+
+console.time('random1')
+let random1 = randomArray1(1, 1e5)
+console.timeEnd('random1')
+console.time('random2')
+let random2 = randomArray2(1, 1e5)
+console.timeEnd('random2')
