ec1c50b35a9630566799c6ca8686d2b3e241f9a9
胡正伟 <331946763@qq.com>
2015-11-22 16:58:48 +0800

    二分查找树

---
+++ src/BinarySearchTree/binarysearchtree.js
@@ -0,0 +1,86 @@
+/**
+ * Created by zwhu on 15/11/22.
+ *
+ * 二分查找树
+ */
+
+import assert from 'assert'
+
+class Node {
+  key
+  val
+  N
+  left  = null
+  right = null
+
+  constructor(key, val, N) {
+    this.key = key
+    this.val = val
+    this.N   = N
+  }
+}
+
+class BinarySearchTree {
+
+  _root = null
+
+  size(node = this._root) {
+    if (!node) return 0
+    return node.N
+  }
+
+  get(key) {
+
+    let _get = (node, key) => {
+
+      if (null === node) return null
+
+      if (key < node.key) return _get(node.left, key)
+      else if (key > node.key) return _get(node.right, key)
+      else return node.val
+
+    }
+
+    return _get(this._root, key)
+  }
+
+  put(key, val) {
+
+    let _put = (node, key, val) => {
+      if (null === node) return new Node(key, val, 1)
+
+      if (key < node.key) node.left = _put(node.left, key, val)
+      else if (key > node.key) node.right = _put(node.right, key, val)
+      else node.val = val
+
+      node.N = this.size(node.left) + this.size(node.right) + 1
+
+      return node
+    }
+
+    this._root = _put(this._root, key, val)
+  }
+
+}
+
+let bst = new BinarySearchTree()
+
+assert.equal(bst.get('a'), null)
+bst.put('a', '1')
+assert.equal(bst.get('a'), '1')
+bst.put('e', '2')
+assert.equal(bst.get('e'), '2')
+bst.put('b', '3')
+assert.equal(bst.get('a'), '1')
+assert.equal(bst.get('b'), '3')
+bst.put('b', '5')
+bst.put('d', '6')
+bst.put('f', '7')
+bst.put('c', '8')
+assert.equal(bst.get('d'), '6')
+assert.equal(bst._root.left, null)
+assert.equal(bst._root.right.key, 'e')
+assert.equal(bst._root.right.right.key, 'f')
+assert.equal(bst._root.right.left.key, 'b')
+assert.equal(bst._root.right.left.right.key, 'd')
+assert.equal(bst._root.right.left.right.left.key, 'c')
